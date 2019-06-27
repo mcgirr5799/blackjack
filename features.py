ae4a9c22f1148bc9ba207371bfea7c54f9bdb090
smcgirr <smcgirr@sfu.ca>
2019-06-26 21:33:01 -0700

    directory change

--- features.py
+++ features.py
@@ -1,3 +1,6 @@
+import deck
+import players
+
 def verification():
     #verification that the input is correct
     while (True):
@@ -7,3 +10,11 @@ def verification():
             return answer
         else:
             print("Invalid data entry, please try again.")
+
+def hit():
+    #function for a hit in blackjack, using multiple classes
+    return
+
+def placeBet():
+    pBet = 0
+    pBet = int(input("Please enter your betting amount: "))
